375e56c6cbfe5c0c0593f2cb983bf835aa3f24a4
Ilya Petrov <ilya.petrov.m@gmail.com>
2020-11-28 23:45:29 +0300

    + Раздел ОЛимпиады скрывается, если ученик не найден на листе + Teams.Подгружаются данные из вкладки + на основе 2 строки подгружаются названия данных lable

--- server.js
+++ server.js
@@ -14,12 +14,46 @@ function getUserEmail() {
     return Session.getActiveUser().getEmail();
 }
 
-function getValueByUrlSRC (url,s,r,c){
-    var nspreadsheet = SpreadsheetApp.openByUrl(url);
-    var ns = nspreadsheet.getSheetByName(s);
-    return ns.getRange(r, c).getValue();
+function getTitelsNums(sh){
+    var head = sh.getRange("1:1").getValues()[0];
+    var titles = {};
+    for(var cell=0; cell < head.length; cell++){
+        if(head[cell] != '')
+          titles[ head[cell] ] = cell+1; // Записываем, какие заголовки у нас есть в таблице и какие номера им присвоены
+    }
+    return titles;
 }
 
+function getFirstInfoByEmail(sh, titles, email){
+    var info = {};
+    
+    var r=0; //номер строки ученика
+    var shLR=sh.getLastRow();
+    var ran = sh.getRange(1,titles['email'],shLR,titles['email']);
+    var data = ran.getValues();
+    var labelRow = 2;
+
+    for (var i = 0; i<shLR; i++) {
+        if (data[i][0] == email) {
+            r=i+1; 
+            var keys = Object.keys(titles);
+            for(var j in keys){
+                info[keys[j]] = {label:""+sh.getRange(labelRow, titles[keys[j]]).getValue(), // label Row
+                                 value:""+sh.getRange(r, titles[keys[j]]).getValue()};
+            }
+            break;
+        }  
+    }
+    return info;
+}
+
+function getTeamsInfo(email){
+    var sh = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1UAEi3OsgwtWncXIOsGvCpNs1O8zFSiE9Vz0ximD9YIA/edit')
+                            .getSheetByName("Teams");
+    var titles = getTitelsNums(sh);
+    var teamsInfo = getFirstInfoByEmail(sh, titles, email);
+    return teamsInfo;
+}
 
 function getDefaultInfo(email){
     var em = email;
